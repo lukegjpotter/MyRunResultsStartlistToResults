53f8e8d5ad4d90d1ec4a06b1f48db7ae0cfd878f
lukegjpotter <lukegjpotter@gmail.com>
2022-12-22 22:44:23 +0000

    Startlist Read Correctly

--- src/main/java/com/lukegjpotter/tools/service/StartlistReaderService.java
+++ src/main/java/com/lukegjpotter/tools/service/StartlistReaderService.java
@@ -1,6 +1,12 @@
 package com.lukegjpotter.tools.service;
 
 import com.lukegjpotter.tools.model.RaceStartlist;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 public class StartlistReaderService {
 
@@ -13,8 +19,16 @@ public class StartlistReaderService {
     public RaceStartlist determineRaceEntrants() {
         RaceStartlist raceStartlist = new RaceStartlist();
 
-        String race = "", firstName = "", surname = "", club = "";
-        raceStartlist.addRiderToStartlist(race, firstName, surname, club);
+        try (CSVReader reader = new CSVReaderBuilder(new FileReader(startlistFileName)).withSkipLines(1).build()) {
+
+            String[] lineInArray;
+
+            while ((lineInArray = reader.readNext()) != null) {
+                raceStartlist.addRiderToStartlist(lineInArray[0], lineInArray[1], lineInArray[2], lineInArray[3]);
+            }
+        } catch (IOException | CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
 
         return raceStartlist;
     }
